9a179da1341462d7230a2a49094b42291d66650f
James Tocknell <aragilar@gmail.com>
2016-02-29 16:28:52 +1100

    Fix read the docs build failure

--- docs/conf.py
+++ docs/conf.py
@@ -16,18 +16,21 @@ import sys
 import os
 import shlex
 
-try:
-    from unittest.mock import MagicMock
-except ImportError:
-    from mock import Mock as MagicMock
+# Avoid build failures on read the docs
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    try:
+        from unittest.mock import MagicMock
+    except ImportError:
+        from mock import Mock as MagicMock
 
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return Mock()
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+                return Mock()
 
-MOCK_MODULES = ['h5py']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+    MOCK_MODULES = ['h5py', 'numpy']
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
