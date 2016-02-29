836d5fd3ac42ce22e5d0b8920e271a510bd6d10c
James Tocknell <aragilar@gmail.com>
2016-02-29 13:55:20 +1100

    Another test for file wrapping

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,11 +1,20 @@
 import pytest
 
-@pytest.mark.xfail
+from h5preserve import H5PreserveFile, new_registry_list
+
 class TestFile(object):
+    @pytest.mark.xfail
     def test_context_manager(self, tmpdir, registry_container):
         assert 0
+    @pytest.mark.xfail
     def test_create(self, tmpdir, registry_container):
         assert 0
+    @pytest.mark.xfail
     def test_close(self, tmpdir, registry_container):
         assert 0
+
+    def test_roundtrip(self, h5py_file):
+        assert h5py_file == H5PreserveFile(
+            h5py_file, new_registry_list()
+        ).h5py_file
 
