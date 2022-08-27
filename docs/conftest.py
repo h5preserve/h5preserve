57f9f836c8a25f80a64c5763b82fc3bb00e6044c
James Tocknell <aragilar@gmail.com>
2022-08-27 13:55:48 +1000

    Fix failing tests

--- docs/conftest.py
+++ docs/conftest.py
@@ -1,14 +1,14 @@
 from doctest import ELLIPSIS
 
 from sybil import Sybil
-from sybil.parsers.codeblock import CodeBlockParser
-from sybil.parsers.doctest import DocTestParser, FIX_BYTE_UNICODE_REPR
+from sybil.parsers.codeblock import PythonCodeBlockParser
+from sybil.parsers.doctest import DocTestParser
 from sybil.parsers.skip import skip
 
 pytest_collect_file = Sybil(
     parsers=[
-        DocTestParser(optionflags=ELLIPSIS|FIX_BYTE_UNICODE_REPR),
-        CodeBlockParser(),
+        DocTestParser(optionflags=ELLIPSIS),
+        PythonCodeBlockParser(),
         skip,
     ],
     fixtures=["tmpdir"],
